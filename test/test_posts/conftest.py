761008aaaae4d1a74c2c5f10b17fed6585db9f86
xionzhi <xizh97@gmail.com>
2022-01-07 17:07:57 +0800

    fix: test client

--- test/test_posts/conftest.py
+++ test/test_posts/conftest.py
@@ -9,26 +9,15 @@
 # Description：
 """
 
-import os
-import tempfile
-
 import pytest
 
 from uuid import uuid4
 from random import randint
 
-from service import app
+from test import client
 
 
-@pytest.fixture
-def client():
-    db_fd, db_path = tempfile.mkstemp()
-
-    with app.test_client() as client:
-        yield client
-
-    os.close(db_fd)
-    os.unlink(db_path)
+client = client
 
 
 hex_uuid = uuid4().hex
--- test/test_users/conftest.py
+++ test/test_users/conftest.py
@@ -9,25 +9,14 @@
 # Description：
 """
 
-import os
-import tempfile
-
 import pytest
 
 from uuid import uuid4
 
-from service import app
+from test import client
 
 
-@pytest.fixture
-def client():
-    db_fd, db_path = tempfile.mkstemp()
-
-    with app.test_client() as client:
-        yield client
-
-    os.close(db_fd)
-    os.unlink(db_path)
+client = client
 
 
 @pytest.fixture()
